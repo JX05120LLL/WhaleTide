d267b76dd08744e0021413436da8ddbbbd9cef0f
YeSKY <ye_sky_personal@foxmail.com>
2025-03-10 18:49:04 +0800

    完成User和Product对应的Mapper基础功能

---
+++ src/main/java/com/macro/mall/tiny/mapper/product/ProductCategoryMapper.java
@@ -0,0 +1,8 @@
+package com.macro.mall.tiny.mapper.product;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.macro.mall.tiny.domain.product.ProductCategory;
+
+public interface ProductCategoryMapper extends BaseMapper<ProductCategory> {
+
+}
---
+++ src/main/java/com/macro/mall/tiny/mapper/product/ProductSkuMapper.java
@@ -0,0 +1,8 @@
+package com.macro.mall.tiny.mapper.product;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.macro.mall.tiny.domain.product.ProductSku;
+
+public interface ProductSkuMapper extends BaseMapper<ProductSku> {
+
+}
